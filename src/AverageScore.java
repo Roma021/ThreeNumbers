2046ca6369733f8453c23d10aa2101b81bb3a266
user.name Roma021 <GBTC441005UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-06-13 16:59:25 -0400

    ThreeNumbers

---
+++ src/AverageScore.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class AverageScore {
+    public static void main(String [] args){
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Exam score:");
+
+        int examScore = Integer.parseInt(reader.nextLine());
+    }
+}
---
+++ src/ThreeNumbers.java
@@ -0,0 +1,13 @@
+
+
+public class ThreeNumbers {
+    public static void main(String [] args){
+
+        int x = 4;
+        int y = 8;
+        int z = 10;
+        int sum = x + y + z;
+
+        System.out.println("The sum of the three numbers is " + sum);
+    }
+}
